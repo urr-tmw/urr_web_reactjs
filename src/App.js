f77993640e9aadfe60f653191b7f511cfcd416cc
rishabh-web-developer <rishabh.k@themailwhale.in>
2025-03-20 11:32:02 +0530

    navBar

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App() {
   return (
     <div className="App">
       {/* <header className="App-header"> */}
-      
+              {/* Content padding */}
+     
       <Navbar/>
 
       <Home />
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,21 @@
 import React, { useState } from "react";
-import { Link,  } from "react-router-dom";
+import { Link, } from "react-router-dom";
 import Enquire from "./Enquire";
 
 const Navbar = () => {
-    return (
-      <>
-        <header className="fixed top-0 left-0 right-0 w-full max-w-[1422px] mx-auto h-[83px] mt-[10px] rounded-[41.26px] bg-white flex items-center justify-between shadow-lg px-4 md:px-8 lg:px-16 z-50 relative">
-        
-          
-        
+  return (
+    <>
+      <header className="fixed top-0 left-0 right-0 w-full max-w-[1422px] mx-auto h-[83px] mt-[10px] rounded-[41.26px] bg-white flex items-center justify-between shadow-lg px-4 md:px-8 lg:px-16 z-50 relative">
 
-        </header>
-  
-        {/* Content padding */}
-        <div className="pt-[83px]" />
-      </>
-    );
-  };
-  
-  export default Navbar;
+
+
+
+      </header>
+
+      {/* Content padding */}
+      <div className="pt-[83px]" />
+    </>
+  );
+};
+
+export default Navbar;
